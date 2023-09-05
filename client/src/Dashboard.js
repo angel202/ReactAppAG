b9804a6076188219a805501b81f87346c780cee1
angel202 <63755749+angel202@users.noreply.github.com>
2023-09-05 15:43:30 -0500

    Playing with api
    Trying to work with spotify-api-web-node

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,7 +1,31 @@
-import React from "react";
+import {useState, useEffect} from 'react';
 import useAuth from './useAuth'
+import SpotifyWebApi from "spotify-web-api-node"
+import { Container, Form } from 'react-bootstrap'
+
+const spotifyApi = new SpotifyWebApi({
+    clientID: "d41293552cb84b3d8839d461cf491f65"
+})
 
 export default function Dashboard({code}){
     const accessToken = useAuth(code)
-    return <div>{code}</div>
+    const [search, setSearch] = useState("")
+    
+    useEffect(() =>{
+        if(!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    },[accessToken])
+
+    
+    return(
+        <Container> 
+            <Form> 
+                <Form.Group>
+                    <Form.Control type="search" placeholder="Search Here" 
+                       value = {search} 
+                        onChange={ e => setSearch(e.target.value)} />
+                </Form.Group >
+            </Form>
+        </Container>
+    ) 
 }
